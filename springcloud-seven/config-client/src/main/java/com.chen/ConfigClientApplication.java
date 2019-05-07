ee19aa05a33f2a375429432104806e437950d80d
chenhao <wb-ch545506@cainiao.com>
2019-05-07 16:35:07 +0800

    seven learn

---
+++ springcloud-seven/config-client/src/main/java/com.chen/ConfigClientApplication.java
@@ -0,0 +1,34 @@
+package com.chen;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//动态刷新，需要post访问http://localhost:8881/actuator/refresh
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RefreshScope
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+    @Value("${democonfigclient.message}")
+    String foo2;
+    @RequestMapping(value = "/hi2")
+    public String hi2(){
+        return foo2;
+    }
+}

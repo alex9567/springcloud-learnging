cd37a65aa15ad6889a2e0b34001f5447c847896e
chenhao <wb-ch545506@cainiao.com>
2019-05-08 16:00:20 +0800

    eight

---
+++ springcloud-eight/config-server/src/main/java/com/chen/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
+//config-client-dev.properties这个文件就访问http://localhost:8888/config-client/dev

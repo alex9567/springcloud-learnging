081090e22b4dd8cfeb23821acf26ff100b7dbed1
“chen <891134726@qq.com>
2019-05-26 22:34:52 +0800

    gateway

---
+++ springcloud-thirteen/spring-gateway3/src/main/java/com/chen/filter/AbstractChangeRequestUriGatewayFilterFactory.java
@@ -0,0 +1,7 @@
+package com.chen.filter;
+
+
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+
+public abstract class AbstractChangeRequestUriGatewayFilterFactory extends AbstractGatewayFilterFactory {
+}

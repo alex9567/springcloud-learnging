226653eed2d532cc4c6b30388d0d1945e85f8aa5
“chen <891134726@qq.com>
2019-06-02 21:46:01 +0800

    hateway

---
+++ springcloud-thirteen/spring-gateway5/src/main/java/com/chen/ServiceGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.chen;
+
+import com.chen.file.RequestTimeGatewayFilterFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceGatewayApplication.class, args );
+    }
+
+    @Bean
+    public RequestTimeGatewayFilterFactory requestTimeGatewayFilterFactory() {
+        return new RequestTimeGatewayFilterFactory();
+    }
+}
